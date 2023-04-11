3dd4187eb5b5d6da0fcba49b1fb3644e4fb768cf
Luis Martinez <rickymtz1999@gmail.com>
2023-04-11 17:11:35 -0500

    adds project

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 let quote = document.getElementById("quote");
 let showQuote = document.getElementById("startQuiz");
 let ca = document.getElementById("character-author");
+let totalContainer = document.getElementById("total-container")
+let next = document.getElementById('next-question')
+let scoreBoard = document.getElementById("score-board")
 
-showQuote.addEventListener('click', cacheApi)
+// showQuote.addEventListener('click',() => {
+//   cacheApi()
+//   showQuote.setAttribute("hidden", "true")
+//   next.removeAttribute("style")
+// })
+
+window.onload= cacheApi()
+
+
+
+let animediv = document.getElementById("quote");
 
 // const _breakingBadArray = await getBreakingBad;
 // const _gameOfThronesArray = await getGameOfThrones;
@@ -20,6 +33,8 @@ let GoTFormatted;
 let shuffledArray;
 let splicedArray;
 
+let count = 0
+let score = 0
 
 //slicing is non mutating while splicing is mutating 
 //splicing cut off section of an array 
@@ -77,17 +92,22 @@ function splicedData () {
     rounds =  shuffledArray.splice(Math.floor(Math.random()*shuffledArray.length),5);
     splicedArray = rounds
     });
-    console.log(shuffleArray(splicedArray))
+    return splicedArray
+    // console.log(shuffleArray(splicedArray))
 }
+
 
 
 async function displayData () {
     splicedArray.forEach((x, index) => {
+      console.log(splicedArray)
     const { author, quote } = x;
     displayQuote.push(quote);
     displayAuthor.push(author);
+    // let test = shuffleArray(displayAuthor)
+    // console.log(test)
+    // console.log(quote)
     let div = document.createElement("div");
-    let animediv = document.getElementById("quote");
     div.innerHTML = `<button class = "characterQuote" id=${author} name = "quote-${index}">${quote}</button>`;
     animediv.appendChild(div);
     let div1 = document.createElement("div");
@@ -144,9 +164,9 @@ function checkAnswer() {
       selectedQuote[0].disabled = true
       quoteIndex = []
       authorIndex = []
-      
+      score++
     }
-  
+    
     else if (isCorrect === false){
       selectedQuote[0].style.backgroundColor = 'crimson'
       authorBtn[0].style.backgroundColor = 'crimson'
@@ -155,8 +175,35 @@ function checkAnswer() {
       quoteIndex = []
       authorIndex = []
     }
-  }
+}
+ 
+next.addEventListener("click", () => {
+  let cQ = document.querySelectorAll(".characterQuote")
+  let newList = splicedData()
+  cQ.forEach((q, index) => {
+    q.setAttribute("style", "background-color: white")
+    q.removeAttribute('disabled')
+    q.innerHTML = `${newList[index].quote}`
+  })
   
+  let auth = document.querySelectorAll(".author")
+  auth.forEach((a, index) => {
+    a.setAttribute("style", "background-color: white")
+    a.removeAttribute('disabled')
+    a.innerHTML = `${newList[index].author}`
+  })
+  count++
+  if (count === 9) {
+    // `<button><a href="./project.html">click here</a></button>`
+    let displayScore = document.createElement('h1')
+    displayScore.innerHTML = `You got a total of ${score} right! If youd like to retake the quiz go to the Home page up above to start again!`
+    scoreBoard.appendChild(displayScore)
+    next.setAttribute('hidden', "true")
+    totalContainer.remove()
+  }
+})
+
+
 
 //* add suffle array into the combined data 
 //* append everthing at once 
@@ -166,10 +213,10 @@ function checkAnswer() {
 //track how many are right 
 
 
-//add score 
-//add feedback
-//style make buttons bigger make text bigger 
-//make responsive 
+//*add score 
+//*add feedback
+//*style make buttons bigger make text bigger 
+//*make responsive 
 //put the buttons above the container 
 //add instructions 
 //get rid of the black line 
@@ -182,3 +229,17 @@ function checkAnswer() {
 // shuffling buttons 
 //add footer 
 //
+
+
+//*on click erase whats in dom 
+//*append the next set of 5 top the dom
+//
+
+
+// *a score to be displayed
+//*once the count reaches 9 disable next button 
+//*display the score 
+
+// screen laod thats sumamary/instructions
+// add btn link to the project html
+// have to have one named index.html
